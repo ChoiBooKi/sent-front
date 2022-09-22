4369475f5829eb0a17097829694065a252e94d86
dorongrong <84131419+dorongrong@users.noreply.github.com>
2022-09-22 20:15:35 +0900

    feat(storybook): Add Simple-Input

---
+++ src/components/SimpleInput.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type InputProps = {
+  title?: string;
+  inputValue?: string;
+};
+
+const SimpleInput = (props: InputProps) => {
+  const { title, inputValue } = props;
+  return <></>;
+};
+
+export default SimpleInput;
---
+++ src/stories/SimpleInput.stories.tsx
@@ -0,0 +1,7 @@
+import SimpleInput from "../components/SimpleInput";
+import React from "react";
+
+export default {
+  title: "Simple-Input",
+  component: SimpleInput,
+};
